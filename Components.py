284daab4eeb022fc6e0a9026be84058a2dd3a877
RA9T0R <Phongphatbangkha@gmail.com>
2024-12-10 14:08:27 +0700

    Update Components.py

--- Components.py
+++ Components.py
@@ -1,26 +1,15 @@
-import altair as alt
 import streamlit as st
 import pandas as pd
-import numpy as np
+ 
+st.write("""
+# This is a Test Components in Streamlit
+Hello *world!*
+""")
+ 
+number = st.slider("Pick a number", 0, 100)
+color = st.color_picker("Pick a color")
+file = st.file_uploader("Pick a file")
 
-from streamlit_vega_lite import vega_lite_component, altair_component
-
-hist_data = pd.DataFrame(np.random.normal(42, 10, (200, 1)), columns=["x"])
-
-@st.cache
-def altair_histogram():
-    brushed = alt.selection_interval(encodings=["x"], name="brushed")
-
-    return (
-        alt.Chart(hist_data)
-        .mark_bar()
-        .encode(alt.X("x:Q", bin=True), y="count()")
-        .add_selection(brushed)
-    )
-
-event_dict = altair_component(altair_chart=altair_histogram())
-
-r = event_dict.get("x")
-if r:
-    filtered = hist_data[(hist_data.x >= r[0]) & (hist_data.x < r[1])]
-    st.write(filtered)
+pets = ["cat", "dog", "fish"]
+pet = st.radio("Pick a pet", pets)
+date = st.date_input("Pick a date")
